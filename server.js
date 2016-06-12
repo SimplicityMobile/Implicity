f0a893d6b055f16f15bdebea889e3bd4d8bebe5e
Edward Jiang <edward@stormpath.com>
2016-06-12 14:00:27 -0700

    initial project structure

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict'
+
+var express = require('express')
+var implicity = require('./index')
+
+var app = express()
+
+app.use('/api', implicity.api)
+app.use('/', implicity.web)
+
+var port = 3000 || process.env.PORT
+app.listen(port, function() {
+	console.log('Implicity server started on port ' + port)
+})
